8a6cecc807edd23ede2eefc698b951c090539841
Max Mitrani <mjmitrani168@gmail.com>
2020-05-22 10:01:48 -0600

    Add movie show spec

---
+++ spec/movies/show_spec.rb
@@ -0,0 +1,21 @@
+describe 'Movies show page', type: :feature do
+  describe "when I visit a movies show page" do
+    it 'shows the movies its details and all its actors statistics' do
+      studio_1 = Studio.create(name: "Megaplex", location: "Utah")
+      studio_2 = Studio.create(name: "Gigaville", location: "Colorado")
+      movie_1 = Movie.create(title: "Goobers", creation_year: "1996", genre: "Horror", studio_id: studio_1.id)
+      actor_1 = Actor.create(name: "Sally Mae", age: "25")
+      actor_2 = Actor.create(name: "Johnny Bee", age: "35")
+
+      visit "/movies/#{movie_1.id}"
+
+      expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_1.creation_year)
+      expect(page).to have_content(movie_1.genre)
+      expect(page).to have_content(actor_1.name)
+      expect(page).to have_content(actor_2.name)
+
+
+    end
+  end
+end
