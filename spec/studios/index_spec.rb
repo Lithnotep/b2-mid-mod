986e33a96c64960e2798a40100f33b83e3ab0a69
Max Mitrani <mjmitrani168@gmail.com>
2020-05-22 09:18:40 -0600

    Add studio index spec

---
+++ spec/studios/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Studios index page' do
+  describe "when I visit '/studios'" do
+    it 'shows a list of all studios and thier movies' do
+      studio_1 = Studio.create(name: "Megaplex", location: "Utah")
+      studio_2 = Studio.create(name: "Gigaville", location: "Colorado")
+      movie_1 = Studio.create(title: "Goobers", creation_year: "1996", genre: "Horror")
+      movie_2 = Studio.create(title: "Slurps", creation_year: "2000", genre: "Rom-Com")
+
+      visit "/studios"
+
+      expect(page).to have_content(studio_1.name)
+      expect(page).to have_content(studio_1.location)
+      expect(page).to have_content(studio_2.name)
+      expect(page).to have_content(studio_2.location)
+      expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_1.creation_year)
+      expect(page).to have_content(movie_1.genre)
+      expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_1.creation_year)
+      expect(page).to have_content(movie_1.genre)
+
+    end
+  end
+end
+
+
+
+
+# <!-- Story 1 -->
+# As a visitor,
+# When I visit the studio index page
+# I see a list of all of the movie studios
+# And underneath each studio, I see the names of all of its movies. %>
