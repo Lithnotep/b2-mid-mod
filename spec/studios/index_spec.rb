aea7f5c47f9b8bac1f94454a868320ade79cfe48
Max Mitrani <mjmitrani168@gmail.com>
2020-05-22 09:42:17 -0600

    add studio controller index method

--- spec/studios/index_spec.rb
+++ spec/studios/index_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe 'Studios index page' do
+describe 'Studios index page', type: :feature do
   describe "when I visit '/studios'" do
     it 'shows a list of all studios and thier movies' do
       studio_1 = Studio.create(name: "Megaplex", location: "Utah")
       studio_2 = Studio.create(name: "Gigaville", location: "Colorado")
-      movie_1 = Studio.create(title: "Goobers", creation_year: "1996", genre: "Horror")
-      movie_2 = Studio.create(title: "Slurps", creation_year: "2000", genre: "Rom-Com")
+      movie_1 = Movie.create(title: "Goobers", creation_year: "1996", genre: "Horror")
+      movie_2 = Movie.create(title: "Slurps", creation_year: "2000", genre: "Rom-Com")
 
       visit "/studios"
 
@@ -17,9 +17,9 @@ describe 'Studios index page' do
       expect(page).to have_content(movie_1.title)
       expect(page).to have_content(movie_1.creation_year)
       expect(page).to have_content(movie_1.genre)
-      expect(page).to have_content(movie_1.title)
-      expect(page).to have_content(movie_1.creation_year)
-      expect(page).to have_content(movie_1.genre)
+      expect(page).to have_content(movie_2.title)
+      expect(page).to have_content(movie_2.creation_year)
+      expect(page).to have_content(movie_2.genre)
 
     end
   end
